5883afc0f43168062b2fbfccfbf92eb120f2df24
shu <shurian1411@gmail.com>
2025-10-27 23:33:01 +0100

    Rewrite main.rs to use clap::Parser.

--- echor/src/main.rs
+++ echor/src/main.rs
@@ -1,36 +1,23 @@
-use clap::{Arg, ArgAction, Command};
+use clap::Parser;
+
+#[derive(Debug, Parser)]
+#[command(author, version, about)]
+/// Rust version of `echo`
+struct Args {
+    /// Input text
+    #[arg(required(true))]
+    text: Vec<String>,
+
+    /// Do not print newline
+    #[arg(short('n'))]
+    omit_newline: bool,
+}
 
 fn main() {
-    // set up a command line interface
-    let matches = Command::new("echor")
-        .version("0.1.0")
-        .author("shu <shurian1411@gmail.com>")
-        .about("Rust version of `echo`")
-        .arg(
-            Arg::new("text")
-                .value_name("TEXT")
-                .help("Input text")
-                .required(true)
-                .num_args(1..),
-        )
-        .arg(
-            Arg::new("omit_newline")
-                .short('n')
-                .action(ArgAction::SetTrue)
-                .help("Do not print newline"),
-        )
-        .get_matches();
-
-    // extract <TEXT> argument(s)
-    // We call unwrap here because we know that the program will require at least one <TEXT>
-    // argument, hence get_many won't return a None.
-    let text: Vec<String> = matches.get_many("text").unwrap().cloned().collect();
-
-    // extract the value of the '-n' flag.
-    let omit_newline = matches.get_flag("omit_newline");
-
-    // omit newline if '-n' flag is present.
-    let ending = if omit_newline { "" } else { "\n" };
-
-    print!("{}{ending}", text.join(" "));
+    let args = Args::parse();
+    print!(
+        "{}{}",
+        args.text.join(" "),
+        if args.omit_newline { "" } else { "\n" }
+    );
 }
