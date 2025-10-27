77eb8c7b1107829496f66369ebd28a57b0217180
shu <shurian1411@gmail.com>
2025-10-27 19:43:25 +0100

    Make tests propagate errors.

--- echor/tests/cli.rs
+++ echor/tests/cli.rs
@@ -1,19 +1,21 @@
+use anyhow::Result;
 use assert_cmd::Command;
 use predicates::prelude::*;
 use std::fs;
 
 #[test]
-fn dies_no_args() {
-    let mut cmd = Command::cargo_bin("echor").unwrap();
+fn dies_no_args() -> Result<()> {
+    let mut cmd = Command::cargo_bin("echor")?;
     cmd.assert()
         .failure()
         .stderr(predicate::str::contains("Usage"));
+    Ok(())
 }
 
 #[test]
-fn hello1() {
-    let outfile = "tests/expected/hello1.txt";
-    let expected = fs::read_to_string(outfile).unwrap();
-    let mut cmd = Command::cargo_bin("echor").unwrap();
+fn hello1() -> Result<()> {
+    let expected = fs::read_to_string("tests/expected/hello1.txt")?;
+    let mut cmd = Command::cargo_bin("echor")?;
     cmd.arg("Hello there").assert().success().stdout(expected);
+    Ok(())
 }
