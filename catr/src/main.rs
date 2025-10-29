b872b197839fdb506d7b8c1468851b646bf9dd14
shu <shurian1411@gmail.com>
2025-10-29 22:16:18 +0100

    Set up `Args` struct and debug print it.

--- catr/src/main.rs
+++ catr/src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+
+#[derive(Debug, Parser)]
+#[command(author, version, about)]
+/// Rust version of `cat`
+struct Args {
+    /// Input file(s)
+    #[arg(default_values_t = ["-".to_string()])]
+    file: Vec<String>,
+
+    /// Number all output lines
+    #[arg(short('n'), long("number"), conflicts_with = "number_nonblank_lines")]
+    number_lines: bool,
+
+    /// Number nonempty output lines
+    #[arg(short('b'), long("number-nonblank"), conflicts_with = "number_lines")]
+    number_nonblank_lines: bool,
 }
+
+fn main() {
+    let args = Args::parse();
+    println!("{args:#?}");
+}
+
