43fedf9bf02b7a9bb904311ecbb9ededafbb5e5b
shu <shurian1411@gmail.com>
2025-11-12 19:30:10 +0100

    Set up CLI parser and output test files

--- headr/src/main.rs
+++ headr/src/main.rs
@@ -1,3 +1,23 @@
+use clap::Parser;
+
+#[derive(Debug, Parser)]
+#[command(author, version, about)]
+/// Rust version of `head`
+struct Args {
+    /// Input file(s)
+    #[arg(value_name = "FILE", default_values_t = ["-".to_string()])]
+    files: Vec<String>,
+
+    /// Number of lines
+    #[arg(short('n'), long("lines"), value_name = "LINES", default_value_t = 10)]
+    lines: u64,
+
+    /// Number of bytes
+    #[arg(short('c'), long("bytes"), value_name = "BYTES")]
+    bytes: Option<u64>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    println!("{:#?}", args);
 }
